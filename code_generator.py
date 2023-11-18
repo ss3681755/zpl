cc1ddaf07784a2f1ab5964a267a2ceaa06de93d5
Shubham Singh <40488764+ss3681755@users.noreply.github.com>
2023-11-18 19:14:31 +0000

    [Refactor] - Moved `stack` size inside the scope along with maintaining `args`.

--- code_generator.py
+++ code_generator.py
@@ -14,8 +14,7 @@ def generate(calls):
         '; -- entrypoint --',
         '_start:',
     ]
-    scope = {}
-    stack = 0
+    scope = {'args': {}, 'stack': 0}
     for c in calls:
         # ['assign', str variable name, int literal | str variable ref]
         if c[0] == 'assign':
@@ -25,11 +24,10 @@ def generate(calls):
                 code.append(f"push {arg2}")
             else:
                 code.append(f"; -- copy {arg2} --")
-                code.append(f"mov rax, [rsp+{8*(stack - scope[arg2] - 1)}]")
-                code.append(f"push rax")
+                code.append(f"push QWORD [rsp+{8*(scope['stack'] - scope['args'][arg2] - 1)}]")
 
-            scope['_'] = scope[arg1] = stack
-            stack += 1
+            scope['args']['_'] = scope['args'][arg1] = scope['stack']
+            scope['stack'] += 1
         # ['add', int literal | str variable ref, int literal | str variable ref]
         elif c[0] == 'add':
             arg1, arg2 = c[1], c[2]
@@ -38,17 +36,17 @@ def generate(calls):
             if isint(arg1):
                 code.append(f"mov rax, {arg1}")
             else:
-                code.append(f"mov rax, [rsp+{8*(stack - scope[arg1] - 1)}]")
+                code.append(f"mov rax, [rsp+{8*(scope['stack'] - scope['args'][arg1] - 1)}]")
 
             if isint(arg2):
                 code.append(f"mov rbx, {arg2}")
             else:
-                code.append(f"mov rbx, [rsp+{8*(stack - scope[arg2] - 1)}]")
+                code.append(f"mov rbx, [rsp+{8*(scope['stack'] - scope['args'][arg2] - 1)}]")
             
             code.append("add rax, rbx")
             code.append("push rax")
-            scope['_'] = stack
-            stack += 1
+            scope['args']['_'] = scope['stack']
+            scope['stack'] += 1
         # ['sub', int literal | str variable ref, int literal | str variable ref]
         elif c[0] == 'sub':
             arg1, arg2 = c[1], c[2]
@@ -57,17 +55,17 @@ def generate(calls):
             if isint(arg1):
                 code.append(f"mov rax, {arg1}")
             else:
-                code.append(f"mov rax, [rsp+{8*(stack - scope[arg1] - 1)}]")
+                code.append(f"mov rax, [rsp+{8*(scope['stack'] - scope['args'][arg1] - 1)}]")
 
             if isint(arg2):
                 code.append(f"mov rbx, {arg2}")
             else:
-                code.append(f"mov rbx, [rsp+{8*(stack - scope[arg2] - 1)}]")
+                code.append(f"mov rbx, [rsp+{8*(scope['stack'] - scope['args'][arg2] - 1)}]")
 
             code.append("sub rax, rbx")
             code.append("push rax")
-            scope['_'] = stack
-            stack += 1
+            scope['args']['_'] = scope['stack']
+            scope['stack'] += 1
         # ['and', int literal | str variable ref, int literal | str variable ref]
         elif c[0] == 'and':
             arg1, arg2 = c[1], c[2]
@@ -76,17 +74,17 @@ def generate(calls):
             if isint(arg1):
                 code.append(f"mov rax, {arg1}")
             else:
-                code.append(f"mov rax, [rsp+{8*(stack - scope[arg1] - 1)}]")
+                code.append(f"mov rax, [rsp+{8*(scope['stack'] - scope['args'][arg1] - 1)}]")
 
             if isint(arg2):
                 code.append(f"mov rbx, {arg2}")
             else:
-                code.append(f"mov rbx, [rsp+{8*(stack - scope[arg2] - 1)}]")
+                code.append(f"mov rbx, [rsp+{8*(scope['stack'] - scope['args'][arg2] - 1)}]")
             
             code.append("and rax, rbx")
             code.append("push rax")
-            scope['_'] = stack
-            stack += 1
+            scope['args']['_'] = scope['stack']
+            scope['stack'] += 1
         # ['or', int literal | str variable ref, int literal | str variable ref]
         elif c[0] == 'or':
             arg1, arg2 = c[1], c[2]
@@ -95,17 +93,17 @@ def generate(calls):
             if isint(arg1):
                 code.append(f"mov rax, {arg1}")
             else:
-                code.append(f"mov rax, [rsp+{8*(stack - scope[arg1] - 1)}]")
+                code.append(f"mov rax, [rsp+{8*(scope['stack'] - scope['args'][arg1] - 1)}]")
 
             if isint(arg2):
                 code.append(f"mov rbx, {arg2}")
             else:
-                code.append(f"mov rbx, [rsp+{8*(stack - scope[arg2] - 1)}]")
+                code.append(f"mov rbx, [rsp+{8*(scope['stack'] - scope['args'][arg2] - 1)}]")
 
             code.append("or rax, rbx")
             code.append("push rax")
-            scope['_'] = stack
-            stack += 1
+            scope['args']['_'] = scope['stack']
+            scope['stack'] += 1
         # ['xor', int literal | str variable ref, int literal | str variable ref]
         elif c[0] == 'xor':
             arg1, arg2 = c[1], c[2]
@@ -114,36 +112,36 @@ def generate(calls):
             if isint(arg1):
                 code.append(f"mov rax, {arg1}")
             else:
-                code.append(f"mov rax, [rsp+{8*(stack - scope[arg1] - 1)}]")
+                code.append(f"mov rax, [rsp+{8*(scope['stack'] - scope['args'][arg1] - 1)}]")
 
             if isint(arg2):
                 code.append(f"mov rbx, {arg2}")
             else:
-                code.append(f"mov rbx, [rsp+{8*(stack - scope[arg2] - 1)}]")
+                code.append(f"mov rbx, [rsp+{8*(scope['stack'] - scope['args'][arg2] - 1)}]")
 
             code.append("xor rax, rbx")
             code.append("push rax")
-            scope['_'] = stack
-            stack += 1
+            scope['args']['_'] = scope['stack']
+            scope['stack'] += 1
         # ['print', int literal | str variable ref]
         elif c[0] == 'print':
             code.append(f"; -- print {c[1]} to console --")
             if isint(c[1]):
                 code.append(f"mov rdi, {c[1]}")
             else:
-                code.append(f"mov rdi, [rsp+{8*(stack - scope[c[1]] - 1)}]")
+                code.append(f"mov rdi, [rsp+{8*(scope['stack'] - scope['args'][c[1]] - 1)}]")
             code.append(f"call _print")
             # system calls do not have any output
-            if '_' in scope : del scope['_']
+            if '_' in scope['args'] : del scope['args']['_']
         elif c[0] == 'exit':
             code.append(f"; -- exit with code {c[1]} --")
             if isint(c[1]):
                 code.append(f"mov rdi, {c[1]}")
             else:
-                code.append(f"mov rdi, [rsp+{8*(stack - scope[c[1]] - 1)}]")
+                code.append(f"mov rdi, [rsp+{8*(scope['stack'] - scope['args'][c[1]] - 1)}]")
             code.append(f"call _exit")
             # system calls do not have any output
-            if '_' in scope : del scope['_']
+            if '_' in scope['args'] : del scope['args']['_']
         else:
             raise Exception(f'Unknow function call {" ".join(map(str, c))}')
     return '\n'.join(code)
