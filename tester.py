0ecc2dc3f309d5520a959e428656ac3155a4e872
Shubham Singh <40488764+ss3681755@users.noreply.github.com>
2023-11-19 09:33:45 +0000

    [Testing] - Fix testing script.

--- tester.py
+++ tester.py
@@ -1,18 +1,13 @@
 import os, subprocess
 from code_generator import generate
 
-for test_file in os.listdir('tests'):
-    if test_file.endswith('.zpl'):
-        expected_output_file = f"tests/{test_file.replace('.zpl', '.out')}"
-        with open(f'tests/{test_file}') as f:
-            program = [line.split() for line in f.read().split('\n') if line]
-            asmcode = generate(program)
-            with open('out.asm', 'w+') as f1:
-                f1.write(asmcode)
-            stdout, _ = subprocess.Popen(['make', 'run'], stdout=subprocess.PIPE).communicate()
-            with open('test.out', 'w+') as f2:
-                f2.write(str(stdout.decode('ascii')))
-            success = os.system(f'diff {expected_output_file} test.out -u') == 0
-            if not success:
-                print(f'Test {test_file} failed.')
-                exit(1)
+for file in os.listdir('tests'):
+    if file.endswith('.zpl'):
+        test_file = f"tests/{file}"
+        output_file = test_file.replace('zpl', 'out')
+        stdout, _ = subprocess.Popen(['make', '-B', test_file], stdout=subprocess.PIPE).communicate()
+        with open('test.out', 'w+') as f2:
+            f2.write(str(stdout.decode('ascii')))
+        success = os.system(f'diff {output_file} test.out -u') == 0
+        if not success: exit(1)
+        else: os.system('make clean')
