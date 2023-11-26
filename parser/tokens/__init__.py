3e2cd8d4f78f4d514bd59c00fec329c22a67f9d0
Shubham Singh <40488764+ss3681755@users.noreply.github.com>
2023-11-26 15:55:24 +0000

    [Tokenizer] - Add a validation mechanism to ensure that all the special char token type are appropriately reverse mapped.

--- parser/tokens/__init__.py
+++ parser/tokens/__init__.py
@@ -10,6 +10,15 @@ class TokenType(Flag):
     TAB = auto()
     UNDERSCORE = auto()
 
+    @classmethod
+    def validate(cls, values):
+        uniq_values = set(values)
+        assert len(values) == len(uniq_values), 'All reverse mapped values must be unique.'
+        for member in cls:
+            if member in [TokenType.ALPHA, TokenType.INTEGER]: continue
+            assert member in uniq_values, 'Make sure all the special character token types are uniquely reverse mapped to a token type.'
+
+
 @dataclass
 class Token:
     start: int
@@ -32,6 +41,8 @@ _SPECIAL_CHAR_TOKEN_TYPE_MAPPING = {
     '-': TokenType.MINUS,
     '_': TokenType.UNDERSCORE
 }
+
+TokenType.validate(_SPECIAL_CHAR_TOKEN_TYPE_MAPPING.values())
 
 def special_char_token_type(value):
     return _SPECIAL_CHAR_TOKEN_TYPE_MAPPING[value]
