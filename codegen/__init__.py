9d12932189316a0d3311173ce6838d0965a3196d
Shubham Singh <40488764+ss3681755@users.noreply.github.com>
2023-12-03 19:25:19 +0000

    [Codegen] - Converted scope from dict to a dataclass.

--- codegen/__init__.py
+++ codegen/__init__.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass, field
+
 from parser.argument import Argument, ArgumentType
 
 X86_64_REGISTERS = ['rax', 'rdi', 'rsi', 'rdx', 'r10', 'r8', 'r9']
@@ -6,20 +8,31 @@ ARITY_1_FUNCTIONS = ['inc', 'dec']
 
 VALUE_ACC = Argument('_', ArgumentType.ATOM)
 
-def deref(scope, arg):
-    return arg if arg.kind == ArgumentType.LITERAL else f"QWORD [rsp+{8*(scope['stack'] - scope['args'][arg] - 1)}]"
+@dataclass
+class Scope:
+    args: dict[Argument, int] = field(default_factory=dict)
+    stack: int = field(default=0)
+
+    def __getitem__(self, arg):
+        match arg.kind:
+            case ArgumentType.LITERAL:
+                return arg
+            case ArgumentType.ATOM:
+                return f"QWORD [rsp+{8*(self.stack - self.args[arg] - 1)}]"
+            case _:
+                raise Exception(f'Unknown argument type {arg}')
 
 def make_name_call(scope, fn, args):
     assert len(args) <= len(X86_64_REGISTERS), f"name only support params upto {len(X86_64_REGISTERS)}."
 
     code = [f"; -- call {fn} with params {' '.join(map(str, args))} --"]
     for reg, arg in zip(X86_64_REGISTERS, args):
-        code.append(f"mov {reg}, {deref(scope, arg)}")
+        code.append(f"mov {reg}, {scope[arg]}")
 
     code.append(f"{fn} {', '.join(X86_64_REGISTERS[:len(args)])}")
     code.append("push rax")
-    scope['args'][VALUE_ACC] = scope['stack']
-    scope['stack'] += 1
+    scope.args[VALUE_ACC] = scope.stack
+    scope.stack += 1
     return code
 
 def generate(calls):
@@ -31,7 +44,7 @@ def generate(calls):
         '; -- entrypoint --',
         '_start:',
     ]
-    scope = {'args': {}, 'stack': 0}
+    scope = Scope()
     index = 0
     while index < len(calls):
         c = calls[index]
@@ -39,10 +52,10 @@ def generate(calls):
         if c.name == 'assign':
             arg1, arg2 = c.args
             code.append(f"; -- {arg1} = {arg2} --")
-            code.append(f"push {deref(scope, arg2)}")
+            code.append(f"push {scope[arg2]}")
 
-            scope['args'][VALUE_ACC] = scope['args'][arg1] = scope['stack']
-            scope['stack'] += 1
+            scope.args[VALUE_ACC] = scope.args[arg1] = scope.stack
+            scope.stack += 1
         # [<fn>, <int | variable>, <int | variable>]
         elif c.name in ARITY_2_FUNCTIONS:
             assert c.argcount() == 2
@@ -56,49 +69,50 @@ def generate(calls):
             # find a way to handle divide by 0 error before generating the asm
             code.append(f"; -- call {c.name} with params {' '.join(map(str, c.args))} --")
             code.append("xor rdx, rdx")
-            code.append(f"mov rax, {deref(scope, arg1)}")
-            code.append(f"mov rdi, {deref(scope, arg2)}")
+            code.append(f"mov rax, {scope[arg1]}")
+            code.append(f"mov rdi, {scope[arg2]}")
             code.append(f"div rdi")
             code.append("push rax")
-            scope['args'][VALUE_ACC] = scope['stack']
-            scope['stack'] += 1
+            scope.args[VALUE_ACC] = scope.stack
+            scope.stack += 1
         elif c.name == 'rem':
             arg1, arg2 = c.args
             # find a way to handle divide by 0 error before generating the asm
             code.append(f"; -- call {c.name} with params {' '.join(map(str, c.args))} --")
             code.append("xor rdx, rdx")
-            code.append(f"mov rax, {deref(scope, arg1)}")
-            code.append(f"mov rdi, {deref(scope, arg2)}")
+            code.append(f"mov rax, {scope[arg1]}")
+            code.append(f"mov rdi, {scope[arg2]}")
             code.append(f"div rdi")
             code.append("push rdx")
-            scope['args'][VALUE_ACC] = scope['stack']
-            scope['stack'] += 1
+            scope.args[VALUE_ACC] = scope.stack
+            scope.stack += 1
         elif c.name == 'mul':
             arg1, arg2 = c.args
             code.append(f"; -- call {c.name} with params {' '.join(map(str, c.args))} --")
-            code.append(f"mov rax, {deref(scope, arg1)}")
-            code.append(f"mov rdi, {deref(scope, arg2)}")
+            code.append(f"mov rax, {scope[arg1]}")
+            code.append(f"mov rdi, {scope[arg2]}")
             code.append(f"mul rdi")
             code.append("push rax")
-            scope['args'][VALUE_ACC] = scope['stack']
-            scope['stack'] += 1
+            scope.args[VALUE_ACC] = scope.stack
+            scope.stack += 1
         # ['print', int literal | str variable ref]
         elif c.name == 'print':
             assert c.argcount() == 1
             arg1 = c.args[0]
             code.append(f"; -- print {arg1} to console --")
-            code.append(f"mov rdi, {deref(scope, arg1)}")
+            code.append(f"mov rdi, {scope[arg1]}")
             code.append(f"call _print")
             # system calls do not have any output
-            if VALUE_ACC in scope['args'] : del scope['args'][VALUE_ACC]
+            # print(scope.args)
+            if VALUE_ACC in scope.args : del scope.args[VALUE_ACC]
         elif c.name == 'exit':
             assert c.argcount() == 1
             arg1 = c.args[0]
             code.append(f"; -- exit with code {arg1} --")
-            code.append(f"mov rdi, {deref(scope, arg1)}")
+            code.append(f"mov rdi, {scope[arg1]}")
             code.append(f"call _exit")
             # system calls do not have any output
-            if VALUE_ACC in scope['args'] : del scope['args'][VALUE_ACC]
+            if VALUE_ACC in scope.args : del scope.args[VALUE_ACC]
         else:
             raise Exception(f'Unknow name call {" ".join(map(str, c))}')
         index += 1
