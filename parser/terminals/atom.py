e4fa95d64fbd86121fd0efd03f0d15a89eea808e
Shubham Singh <40488764+ss3681755@users.noreply.github.com>
2023-12-24 14:39:16 +0000

    [Terminals] - Transform token inside the parse function itself.

--- parser/terminals/atom.py
+++ parser/terminals/atom.py
@@ -4,27 +4,19 @@ def _parse_underscores(cursor):
     while cursor.can_move() and cursor.peek().token_type == TokenType.UNDERSCORE:
         cursor.move()
 
-def _join_underscores(cursor):
-    if tokens := cursor.attempt(_parse_underscores):
-        return ''.join([t.value for t in tokens])
-
 def _parse_alphabets(cursor):
     while cursor.can_move() and cursor.peek().token_type == TokenType.ALPHA:
         cursor.move()
-
-def _join_alphabets(cursor):
-    if tokens := cursor.attempt(_parse_alphabets):
-        return ''.join([t.value for t in tokens])
 
 # an atom is valid placeholder with regex [_a-zA-Z]+
 def parse_atom(cursor):
     name = []
     while cursor.can_move():
-        if underscores := cursor.attempt(_join_underscores):
-            name.append(underscores)
+        if underscores := cursor.attempt(_parse_underscores):
+            name.extend(underscores)
+        elif alphabets := cursor.attempt(_parse_alphabets):
+            name.extend(alphabets)
+        else:
+            break
 
-        if alphabets := cursor.attempt(_join_alphabets):
-            name.append(alphabets)
-
-        if underscores is None and alphabets is None: break
-    return ''.join(name) or None
+    return ''.join(map(lambda x: x.value, name))
--- parser/terminals/empty_line.py
+++ parser/terminals/empty_line.py
@@ -8,19 +8,8 @@ def _parse_newlines(cursor):
     while cursor.can_move() and cursor.peek().token_type == TokenType.NEWLINE:
         cursor.move()
 
-def _count_spaces(cursor):
-    if spaces := cursor.attempt(_parse_spaces):
-        return len(spaces)
-    return 0
-
-def _count_newlines(cursor):
-    if newlines := cursor.attempt(_parse_newlines):
-        return len(newlines)
-    return 0
-
 def parse_empty_lines(cursor):
-    spaces = cursor.attempt(_count_spaces)
-    newlines = cursor.attempt(_count_newlines)
-    while cursor.can_move() and (spaces > 0 or newlines > 0):
-        spaces = cursor.attempt(_count_spaces)
-        newlines = cursor.attempt(_count_newlines)
+    while cursor.can_move():
+        spaces = cursor.attempt(_parse_spaces)
+        newlines = cursor.attempt(_parse_newlines)
+        if spaces is None and newlines is None: break
